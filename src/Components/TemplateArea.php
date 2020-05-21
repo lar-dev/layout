e13145f4383bcd92317a8e70a92adb736b604718
xsaven <gms.sw.xsaven@gmail.com>
2020-05-21 19:35:49 +0300

    Changed: src/Components/TemplateArea.php

--- src/Components/TemplateArea.php
+++ src/Components/TemplateArea.php
@@ -14,13 +14,23 @@ class TemplateArea extends SPAN
     /**
      * Col constructor.
      * @param  string|null  $id
+     * @param  bool  $autoload
      * @param  mixed  ...$params
      */
-    public function __construct(string $id, ...$params)
+    public function __construct(string $id, bool $autoload = false, ...$params)
     {
         parent::__construct();
 
-        $this->setDatas(['tpl' => $id]);
+        $data = ['tpl' => $id];
+
+        if ($autoload) {
+
+            $data['load'] = 'tpl::replaceTo';
+
+            $data['load-params'] = $id;
+        }
+
+        $this->setDatas($data);
 
         $this->when($params);
     }
