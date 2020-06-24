bfc2422742abe706f6fcc1b61044b1dcaba359c6
xsaven <gms.sw.xsaven@gmail.com>
2020-06-24 22:42:41 +0300

    Changed: src/Tags/I.php, src/Traits/FontAwesome.php

--- src/Tags/I.php
+++ src/Tags/I.php
@@ -3,13 +3,27 @@
 namespace Lar\Layout\Tags;
 
 use Lar\Layout\Abstracts\Component;
+use Lar\Layout\Traits\FontAwesome;
 
 class I extends Component
 {
+    use FontAwesome;
+
     /**
      * Tag element
      *
      * @var string
      */
     protected $element = "i";
+
+    /**
+     * @param  string  $icon
+     * @return $this
+     */
+    public function icon(string $icon)
+    {
+        $this->attr('class', $icon);
+
+        return $this;
+    }
 }
