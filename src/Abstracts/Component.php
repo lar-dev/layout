681cc1923c844a801447a96a2a71c75f42bdcbfb
xsaven <gms.sw.xsaven@gmail.com>
2020-07-02 23:15:59 +0300

    Changed: src/Abstracts/Component.php

--- src/Abstracts/Component.php
+++ src/Abstracts/Component.php
@@ -145,27 +145,4 @@ class Component extends ComponentStatic
     {
         return $this->render();
     }
-
-    /**
-     * @param  string  $name
-     * @return \ReflectionFunction|null
-     * @throws \ReflectionException
-     */
-    public static function get_macro_reflex(string $name)
-    {
-        if (isset(static::$macros[$name])) {
-
-            return new \ReflectionFunction(static::$macros[$name]);
-        }
-
-        return null;
-    }
-
-    /**
-     * @return array
-     */
-    public static function get_macro_names()
-    {
-        return array_keys(static::$macros);
-    }
 }
